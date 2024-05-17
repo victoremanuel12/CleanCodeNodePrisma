7d031e080d47ecde58a22d05517a17f5811a1d24
Victor Emanuel <victoremannuel12@hotmail.com>
2024-05-17 10:21:32 -0300

    caso de uso Criação de produto finalizado

--- src/UsesCases/product/create-product.usecase.ts
+++ src/UsesCases/product/create-product.usecase.ts
@@ -1,11 +1,27 @@
-import { Usecase } from "../usecase"
-
+import { Usecase } from "../Usecase";
+import { ProductGeteway } from "../../Domain/Geteways/products.geteway";
+import { Product } from "../../Domain/Entity/product.entity";
 export type createProductInputDto = {
-
-}
+  name: string;
+  price: number;
+};
 export type CreateProductOutputDto = {
+  id: string;
+};
 
-}
-export class CreateProductUsecase implements Usecase<createProductInputDto,CreateProductOutputDto> { 
-    
+export class CreateProductUsecase
+  implements Usecase<createProductInputDto, CreateProductOutputDto>
+{
+  private constructor(private readonly productGateway : ProductGeteway) {}
+  public static create(productGeteway: ProductGeteway) {
+    return new CreateProductUsecase(productGeteway);
+  }
+  public async execute({name, price}: createProductInputDto): Promise<CreateProductOutputDto> {
+        const addProduct = Product.create(name, price);
+        await this.productGateway.save(addProduct);
+        const output : CreateProductOutputDto = {
+            id: addProduct.id
+        }
+        return output;
+  }
 }
